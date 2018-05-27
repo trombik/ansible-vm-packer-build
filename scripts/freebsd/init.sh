baea544d2ac21908b295a8113b16e71131f91b3a
Tomoyuki Sakurai <240118+trombik@users.noreply.github.com>
2018-05-28 00:08:12 +0900

    [feature] run freebsd-update (#37)
    * [feature] run freebsd-update
    
    * [bugfix] rubocop

--- scripts/freebsd/init.sh
+++ scripts/freebsd/init.sh
@@ -12,3 +12,15 @@ ${fwcmd} -f flush
 ${fwcmd} check-state
 ${fwcmd} add 65000 pass all from any to any keep-state
 EOF
+
+case `uname -r` in
+    10.3-*)
+        # XXX when EoLed, freebsd-update exits with zero
+        sudo freebsd-update --not-running-from-cron fetch || true
+        ;;
+    *)
+        sudo freebsd-update --not-running-from-cron fetch
+        ;;
+esac
+
+sudo freebsd-update --not-running-from-cron install
---
+++ spec/security_patch_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+case os[:family]
+when "freebsd"
+  describe command "freebsd-update --not-running-from-cron fetch" do
+    # XXX os[:release] does not report proper value when `uname -r` contains
+    # patche-level. use uname -K instead
+    kernel_version = Specinfra.backend.run_command("uname -K").stdout.to_i
+    if kernel_version < 1_004_000
+
+      # `freebsd-update` exits with non-zero on EoLed releases
+      its(:exit_status) { should eq 1 }
+    else
+      its(:exit_status) { should eq 0 }
+    end
+    its(:stderr) { should eq "" }
+    its(:stdout) { should match(/^No updates needed to update system to/) }
+  end
+
+  describe command "freebsd-update --not-running-from-cron install" do
+    its(:exit_status) { should eq 1 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should match(/^No updates are available to install/) }
+  end
+end
