c774fe3e0e25c0f4459844c5118092fb91748c70
Tomoyuki Sakurai <y@trombik.org>
2018-05-27 23:55:05 +0900

    [bugfix] rubocop

--- spec/security_patch_spec.rb
+++ spec/security_patch_spec.rb
@@ -3,11 +3,10 @@ require "spec_helper"
 case os[:family]
 when "freebsd"
   describe command "freebsd-update --not-running-from-cron fetch" do
-
     # XXX os[:release] does not report proper value when `uname -r` contains
     # patche-level. use uname -K instead
     kernel_version = Specinfra.backend.run_command("uname -K").stdout.to_i
-    if kernel_version < 1004000
+    if kernel_version < 1_004_000
 
       # `freebsd-update` exits with non-zero on EoLed releases
       its(:exit_status) { should eq 1 }
