6374d09833fd2fc07129b93c96c763f5e2e12373
Tomoyuki Sakurai <y@trombik.org>
2019-05-03 14:43:08 +0900

    bugfix: test python symlinks only on older releases

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -21,14 +21,6 @@ when "freebsd"
     it { should be_installed }
   end
 when "openbsd"
-  # XXX RE_5_9 does not have the latest ansible yet
-  if os[:release].to_f >= 6.0
-    describe command "ansible --version" do
-      its(:exit_status) { should eq 0 }
-      its(:stderr) { should eq "" }
-      its(:stdout) { should match(/^ansible\s+2\.5\.\d+(\.\d+)?/) }
-    end
-  end
   if os[:release].to_f >= 6.1
     describe file("/etc/pkg.conf") do
       it { should_not exist }
@@ -47,25 +39,28 @@ when "openbsd"
     end
   end
 
-  prefix = "/usr/local/bin"
-  python_version = "2.7"
-  python_symlinks = {
-    "#{prefix}/python" => "#{prefix}/python#{python_version}",
-    "#{prefix}/2to3" => "#{prefix}/python#{python_version}-2to3",
-    "#{prefix}/python-config" => "#{prefix}/python#{python_version}-config",
-    "#{prefix}/pydoc" => "#{prefix}/pydoc#{python_version}"
-  }
-  python_symlinks.each do |k, v|
-    describe file(k) do
-      it { should exist }
-      it { should be_symlink }
-      it { should be_linked_to v }
-    end
+  # before 6.5, the default python is 2.x
+  if os[:release].to_f < 6.5
+    prefix = "/usr/local/bin"
+    python_version = "2.7"
+    python_symlinks = {
+      "#{prefix}/python" => "#{prefix}/python#{python_version}",
+      "#{prefix}/2to3" => "#{prefix}/python#{python_version}-2to3",
+      "#{prefix}/python-config" => "#{prefix}/python#{python_version}-config",
+      "#{prefix}/pydoc" => "#{prefix}/pydoc#{python_version}"
+    }
+    python_symlinks.each do |k, v|
+      describe file(k) do
+        it { should exist }
+        it { should be_symlink }
+        it { should be_linked_to v }
+      end
 
-    describe file(v) do
-      it { should exist }
-      it { should be_file }
-      it { should be_mode os[:release].to_f >= 6.0 ? 755 : 555 }
+      describe file(v) do
+        it { should exist }
+        it { should be_file }
+        it { should be_mode os[:release].to_f >= 6.0 ? 755 : 555 }
+      end
     end
   end
   if os[:release].to_f >= 6.0
