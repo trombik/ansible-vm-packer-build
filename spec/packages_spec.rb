bbeb40764697606cc23056c7c7968ed968f0e362
Tomoyuki Sakurai <y@trombik.org>
2019-11-20 20:54:09 +0900

    bugfix: remove tests for unsupported versions

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -22,16 +22,14 @@ when "freebsd"
   end
 when "openbsd"
   kern_version = Specinfra.backend.run_command("sysctl -n kern.version").stdout
-  if os[:release].to_f >= 6.1
-    describe command("syspatch -c") do
-      if kern_version =~ /-(current|beta)/
-        its(:exit_status) { should eq 1 }
-        its(:stderr) { should match(/^Unsupported release: \d+\.\d+-(current|beta)/) }
-      else
-        its(:exit_status) { should eq 0 }
-        its(:stderr) { should eq "" }
-        its(:stdout) { should eq "" }
-      end
+  describe command("syspatch -c") do
+    if kern_version =~ /-(current|beta)/
+      its(:exit_status) { should eq 1 }
+      its(:stderr) { should match(/^Unsupported release: \d+\.\d+-(current|beta)/) }
+    else
+      its(:exit_status) { should eq 0 }
+      its(:stderr) { should eq "" }
+      its(:stdout) { should eq "" }
     end
   end
 end
