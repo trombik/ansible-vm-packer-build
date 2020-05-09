02ebab1997c4145b762919ffec439c8ee877b217
Tomoyuki Sakurai <y@trombik.org>
2020-05-09 18:30:51 +0900

    bugfix: add my local package tree for ansible 2.9.x

--- scripts/freebsd/init.sh
+++ scripts/freebsd/init.sh
@@ -13,11 +13,25 @@ ${fwcmd} check-state
 ${fwcmd} add 65000 pass all from any to any keep-state
 EOF
 
+# Install the latest ansible 2.9.x from my local package tree.
+sudo mkdir -p /usr/local/etc/pkg/repos
+# 130, 123, etc
+version_short=`uname -r | sed -E -e 's/-(CURRENT|RELEASE)//' -e 's/[.]//' | tr -d '\n'`
+machine=`uname -m`
+sudo tee /usr/local/etc/pkg/repos/local.conf <<__EOF__
+devel: {
+  url             : "http://pkg.i.trombik.org/$version_short$machine-default-default",
+  enabled         : yes,
+  priority        : 100
+}
+__EOF__
+
 # XXX install python first so that `/usr/local/bin/python` works. when python
 # is installed as a dependency of `ansible`, `/usr/local/bin/python$X.$Y`,
 # where $X and $Y is python version number, is installed.
 # XXX after FLAVORed ansible, `sysutils/` prefix is required.
 sudo pkg install -y lang/python3 py37-ansible rsync
+sudo rm /usr/local/etc/pkg/repos/local.conf
 
 # adjust date before freebsd-update. incorrect time causes freebsd-update to
 # log "expr: illegal option" to stderr, which causes a failure in tests
