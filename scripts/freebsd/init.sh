5cc3c684b396cf464e8f0a715d77bcb8f7a3219e
Tomoyuki Sakurai <y@trombik.org>
2020-05-15 09:37:24 +0900

    bugfix: disable internal repo for the moment

--- scripts/freebsd/init.sh
+++ scripts/freebsd/init.sh
@@ -18,20 +18,20 @@ sudo mkdir -p /usr/local/etc/pkg/repos
 # 130, 123, etc
 version_short=`uname -r | sed -E -e 's/-(CURRENT|RELEASE)//' -e 's/[.]//' | tr -d '\n'`
 machine=`uname -m`
-sudo tee /usr/local/etc/pkg/repos/local.conf <<__EOF__
-devel: {
-  url             : "http://pkg.i.trombik.org/$version_short$machine-default-default",
-  enabled         : yes,
-  priority        : 100
-}
-__EOF__
+# sudo tee /usr/local/etc/pkg/repos/local.conf <<__EOF__
+# devel: {
+#   url             : "http://pkg.i.trombik.org/$version_short$machine-default-default",
+#   enabled         : yes,
+#   priority        : 100
+# }
+# __EOF__
 
 # XXX install python first so that `/usr/local/bin/python` works. when python
 # is installed as a dependency of `ansible`, `/usr/local/bin/python$X.$Y`,
 # where $X and $Y is python version number, is installed.
 # XXX after FLAVORed ansible, `sysutils/` prefix is required.
 sudo pkg install -y lang/python3 py37-ansible rsync
-sudo rm /usr/local/etc/pkg/repos/local.conf
+sudo rm -f /usr/local/etc/pkg/repos/local.conf
 
 
 # adjust date before freebsd-update. incorrect time causes freebsd-update to
